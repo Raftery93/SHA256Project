1bf2f8a0801ca6d45e8e04f73d1a69e3c11ca7b4
Conor Raftery <c-raftery@hotmail.com>
2019-03-14 22:19:22 +0000

    Manipulating final block of bits in file

--- padfile.c
+++ padfile.c
@@ -19,6 +19,7 @@ int main(int argc, char *argv[]){
 
     //Number of bytes
     uint64_t nobytes;
+    uint64_t nobits = 0;
 
     //Create file pointer
     FILE* f;
@@ -29,9 +30,19 @@ int main(int argc, char *argv[]){
     //Loop until end of file
     while(!feof(f)){
 
-    //Read in bits into 64 bits
-    nobytes = fread(M.e, 1, 64, f);
-    
+        //Read in bits into 64 bits
+        nobytes = fread(M.e, 1, 64, f);
+        nobits = nobits + (nobytes + 8);
+        if(nobytes < 56){
+            printf("I've found a block with less than 55 bytes!\n");
+            M.e[nobytes] = 0x80;
+            while(nobytes < 56){
+                nobytes = nobytes + 1;
+                M.e[nobytes] = 0x00;
+            }
+            M.s[7] = nobits;
+        }
+
     //Display how many bits are read in
     printf("%llu\n", nobytes);
     }
