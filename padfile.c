127522b71f60aa94bc3997f2ae4478719a210505
Conor Raftery <c-raftery@hotmail.com>
2019-03-14 21:00:16 +0000

    Video 3 finished

--- padfile.c
+++ padfile.c
@@ -5,6 +5,7 @@
 //Fixed bit length integers
 #include <stdint.h>
 
+//Creates a union for storing certain size bits
 union msgblock {
     uint8_t e[64];
     uint32_t t[16];
@@ -13,19 +14,29 @@ union msgblock {
 
 int main(int argc, char *argv[]){
     
+    //Declare union
     union msgblock M;
 
+    //Number of bytes
     uint64_t nobytes;
 
+    //Create file pointer
     FILE* f;
 
+    //Open file for reading (1st command line argument)
     f = fopen(argv[1], "r");
 
+    //Loop until end of file
     while(!feof(f)){
+
+    //Read in bits into 64 bits
     nobytes = fread(M.e, 1, 64, f);
+    
+    //Display how many bits are read in
     printf("%llu\n", nobytes);
     }
 
+    //Close file
     fclose(f);
 
     return 0;
